41c1b597e434fd6b65defd29b7855834da1d201f
Jayam Milesh Mehta <jayammehta2711@gmail.com>
2026-01-02 20:18:45 +0530

    commit from Jayam

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85a2274c9a3c50f20dde961b03d1cd2a8c2a4c104f9fa4c06c91780c770f52ce
-size 548835
+oid sha256:b0765346ceb6b8400f2bcae3b52ecb6fe3f30ab485f7cc8743b1caff04c10945
+size 547729
--- backend/scanners/authentication_session_management/auth_session_scanner.py
+++ backend/scanners/authentication_session_management/auth_session_scanner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ed45d8084c4a0e3f83ca12190823065fcc5005b4f77ba3fd0f1e8e7f30c2b62
-size 83965
+oid sha256:d4f871210033efa4f3da8a1dbb655b81d9c2c87716b63afdfe0425d099ca173d
+size 96733
